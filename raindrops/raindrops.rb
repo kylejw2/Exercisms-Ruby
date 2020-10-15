44b73ea848352ee462c2cddb9a11138798fdd425
Kyle Wright <kylejamwright@gmail.com>
2020-10-15 15:16:44 -0600

    Exercism #9

---
+++ raindrops/raindrops.rb
@@ -0,0 +1,18 @@
+module Raindrops
+    def self.convert(num)
+        str = ""
+        if num % 3 == 0
+            str += "Pling"
+        end
+        if num % 5 == 0
+            str += "Plang"
+        end
+        if num % 7 == 0
+            str += "Plong"
+        end
+        if str == ""
+            str = num.to_s
+        end
+        return str
+    end
+end
